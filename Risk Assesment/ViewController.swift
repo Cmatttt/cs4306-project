2403c3e1edd76faaacbac9733b7a1942cb98af99
Cmatttt <50069720+Cmatttt@users.noreply.github.com>
2020-11-12 11:16:24 -0600

    11/11 Changes

--- Risk Assesment/ViewController.swift
+++ Risk Assesment/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     //Initialize map and label
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var types: UILabel!
+    @IBOutlet weak var btn1: UIButton!
+    
     
     private var placesClient: GMSPlacesClient!
 
@@ -30,6 +33,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         placesClient = GMSPlacesClient.shared()
         
+        btn1.layer.cornerRadius = 6
+        
         //Get best accuracy
         locman.desiredAccuracy = kCLLocationAccuracyBest // Can drain battery??
         //Request location access
@@ -39,8 +44,12 @@ class ViewController: UIViewController {
         locman.startUpdatingLocation()
     }
     
+    //let county = locale.regionCode
+
     @IBAction func getCurrentPlace(_ sender: UIButton) {
         
+        //let locale = Locale.current
+        //let county = locale.regionCode
         //set feild to type
         let placeFields: GMSPlaceField = .types
             placesClient.findPlaceLikelihoodsFromCurrentLocation(withPlaceFields: placeFields) { [weak self] (placeLikelihoods, error) in
@@ -55,13 +64,71 @@ class ViewController: UIViewController {
 
               guard let place = placeLikelihoods?.first?.place else {
                 strongSelf.nameLabel.text = "No current place"
-                //strongSelf.adressLabel.text = ""
                 return
               }
                 
-                // Display type
-                strongSelf.nameLabel.text = place.types?.last
-              //strongSelf.adressLabel.text = place.formattedAddress
+
+                let allPlaces = place.types
+                
+                let allp = place.types?.joined(separator: ",")
+                
+                print(allPlaces)
+                
+                
+                var singlePlace = ""
+                
+                if let unwrapped = allPlaces {
+                    let p = unwrapped.joined(separator: ",")
+                    
+                    let pArray = p.components(separatedBy: ",")
+                    
+                    //print(type(of: pArray))
+
+                    if pArray.contains("university"){
+                        singlePlace = "university"
+                    } else if pArray.contains("gas_station"){
+                        singlePlace = "gas station"
+                    } else if pArray.contains("restaurant"){
+                        singlePlace = "restaurant"
+                    } else if pArray.contains("car_dealer"){
+                        singlePlace = "dealership"
+                    } else if pArray.contains("lodging"){
+                        singlePlace = "hotel"
+                    } else if pArray.contains("hospital"){
+                        singlePlace = "hospital"
+                    } else if pArray.contains("gym"){
+                        singlePlace = "gym"
+                    } else if pArray.contains("airport"){
+                        singlePlace = "airport"
+                    } else if pArray.contains("bank"){
+                        singlePlace = "bank"
+                    } else if pArray.contains("church"){
+                        singlePlace = "church"
+                    } else if pArray.contains("doctor"){
+                        singlePlace = "doctor"
+                    } else if pArray.contains("stadium"){
+                        singlePlace = "stadium"
+                    } else if pArray.contains("supermarket"){
+                        singlePlace = "supermarket"
+                    } else if pArray.contains("store"){
+                        singlePlace = "store"
+                    } else if pArray.contains("establishment"){
+                        singlePlace = "establishment"
+                    } else if pArray.contains("museum"){
+                        singlePlace = "museum"
+                    }
+
+                    
+                    
+                } else {
+                    print("Missing name.")
+                }
+                
+                print(singlePlace)
+                
+                strongSelf.nameLabel.text = singlePlace
+                strongSelf.types.text = allp
+                
         }
     }
 }
