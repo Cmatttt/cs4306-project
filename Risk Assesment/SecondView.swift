be4c6bb6890cf1c4a878b0696453a1c266cc415b
UgoEkwe <39320450+UgoEkwe@users.noreply.github.com>
2020-11-28 12:56:24 -0600

    Update SecondView.swift
    Removed redundancy and added default result to risk display

--- Risk Assesment/SecondView.swift
+++ Risk Assesment/SecondView.swift
@@ -3,6 +3,7 @@
 //  Risk Assesment
 //
 //  Created by cmatt on 11/22/20.
+//  Edited by ugoekwe on 11/28/20
 //  Copyright © 2020 Cmatt. All rights reserved.
 //
 
@@ -19,6 +20,7 @@ class SecondView: UIViewController {
     var county: String = ""
     var place: String = ""
     var pulseLayer: CAShapeLayer!
+    var risk: String = ""
 
     //initializing labels and buttons
     @IBOutlet var SecondView: UIView!
@@ -94,133 +96,75 @@ class SecondView: UIViewController {
         if percentage <= 0.10 {
             //display low risk
             lol.text = "Low Risk"
+            risk = "low risk"
             shapeLayer.strokeColor = UIColor.green.cgColor
             pulseLayer.strokeColor = UIColor.green.cgColor
             pulseLayer.shadowColor = UIColor.green.cgColor
             pulsate()
             view.layer.addSublayer(pulseLayer)
             view.layer.addSublayer(shapeLayer)
-            //display what type of place the user is in and tell precautions
-            if place == "university"{
-                riskkResult.text = "You're at a university in a low risk area, please be sure to follow campus COVID-19 guidlines."
-            } else if place == "gas station"{
-                riskkResult.text = "You're at a gas station in a low risk area, please still be cautious and sanitize your hands after touching pumps or purchasing items from inside."
-            } else if place == "restaurant"{
-                riskkResult.text = "You're at a restaurant in a low risk area, please keep socially distanced from others and wear a mask when speaking to employees and others."
-            } else if place == "dealership"{
-                riskkResult.text = "You're at a dealership in a low risk area, please wear a mask at all times and santitize hands often."
-            } else if place == "hotel"{
-                riskkResult.text = "You're at a hotel in a low risk area, please wear a mask when you are not in your and sanitize often."
-            } else if place == "hospital"{
-                riskkResult.text = "You're at a hospital in a low risk area, please be cautious and follow hospital COVID-19 guidlines as this are may still be high risk."
-            } else if place == "gym"{
-                riskkResult.text = "You're at a gym in a low risk area, please still be sure to take normal covid precautions."
-            } else if place == "airport"{
-                riskkResult.text = "You're at a airport in a low risk area, please be cautious and follow airport COVID-19 guidlines as this are may still be high risk."
-            } else if place == "bank"{
-                riskkResult.text = "You're at a bank in a low risk area, please keep socially distanced from others and wear a mask when speaking to employees and others."
-            } else if place == "church"{
-                riskkResult.text = "You're at a church in a low risk area, please wear a mask at all times and socially distace from others while keeping hands sanitized."
-            } else if place == "doctor"{
-                riskkResult.text = "You're at a doctor in a low risk area, please be cautious and follow companies COVID-19 guidlines as this are may still be high risk."
-            } else if place == "stadium"{
-                riskkResult.text = "You're at a stadium in a low risk area, please keep socially ditanced and santitize often."
-            } else if place == "supermarket"{
-                riskkResult.text = "You're at a supermarket in a low risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            } else if place == "store"{
-                riskkResult.text = "You're at a store in a low risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            } else if place == "establishment"{
-                riskkResult.text = "You're in an establishment in a low risk area, please keep socially distanced and keep hands sanitized."
-            } else if place == "museum"{
-                riskkResult.text = "You're at a museum in a low risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            }
-        } else if percentage > 0.1 && percentage < 0.40 {
+        }
+        
+        else if percentage > 0.1 && percentage < 0.40 {
             //display medium risk
             lol.font = UIFont.boldSystemFont(ofSize: 24)
             lol.text = "Medium Risk"
+            risk = "medium risk"
             shapeLayer.strokeColor = UIColor.yellow.cgColor
             pulseLayer.strokeColor = UIColor.yellow.cgColor
             pulseLayer.shadowColor = UIColor.yellow.cgColor
             pulsate()
             view.layer.addSublayer(pulseLayer)
             view.layer.addSublayer(shapeLayer)
-            //display what type of place the user is in and tell precautions
-            if place == "university"{
-                riskkResult.text = "You're at a university in a medium risk area, please be sure to follow campus COVID-19 guidlines."
-            } else if place == "gas station"{
-                riskkResult.text = "You're at a gas station in a medium risk area, please still be cautious and sanitize your hands after touching pumps or purchasing items from inside."
-            } else if place == "restaurant"{
-                riskkResult.text = "You're at a restaurant in a medium risk area, please keep socially distanced from others and wear a mask when speaking to employees and others."
-            } else if place == "dealership"{
-                riskkResult.text = "You're at a dealership in a medium risk area, please wear a mask at all times and santitize hands often."
-            } else if place == "hotel"{
-                riskkResult.text = "You're at a hotel in a medium risk area, please wear a mask when you are not in your and sanitize often."
-            } else if place == "hospital"{
-                riskkResult.text = "You're at a hospital in a medium risk area, please be cautious and follow hospital COVID-19 guidlines as this are may still be high risk."
-            } else if place == "gym"{
-                riskkResult.text = "You're at a gym in a medium risk area, please still be sure to take normal covid precautions."
-            } else if place == "airport"{
-                riskkResult.text = "You're at a airport in a medium risk area, please be cautious and follow airport COVID-19 guidlines as this are may still be high risk."
-            } else if place == "bank"{
-                riskkResult.text = "You're at a bank in a medium risk area, please keep socially distanced from others and wear a mask when speaking to employees and others."
-            } else if place == "church"{
-                riskkResult.text = "You're at a church in a medium risk area, please wear a mask at all times and socially distace from others while keeping hands sanitized."
-            } else if place == "doctor"{
-                riskkResult.text = "You're at a doctor in a medium risk area, please be cautious and follow companies COVID-19 guidlines as this are may still be high risk."
-            } else if place == "stadium"{
-                riskkResult.text = "You're at a stadium in a medium risk area, please keep socially ditanced and santitize often."
-            } else if place == "supermarket"{
-                riskkResult.text = "You're at a supermarket in a medium risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            } else if place == "store"{
-                riskkResult.text = "You're at a store in a medium risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            } else if place == "establishment"{
-                riskkResult.text = "You're in an establishment in a medium risk area, please keep socially distanced and keep hands sanitized."
-            } else if place == "museum"{
-                riskkResult.text = "You're at a museum in a medium risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            }
-        } else if percentage > 0.40 {
+        }
+        
+        else if percentage > 0.40 {
             //display high risk
             lol.text = "High Risk"
+            risk = "high risk"
             shapeLayer.strokeColor = UIColor.red.cgColor
             pulseLayer.strokeColor = UIColor.red.cgColor
             pulseLayer.shadowColor = UIColor.red.cgColor
             pulsate()
             view.layer.addSublayer(pulseLayer)
             view.layer.addSublayer(shapeLayer)
-            //display what type of place the user is in and tell precautions
-            if place == "university"{
-                riskkResult.text = "You're at a university in a high risk area, please be sure to follow campus COVID-19 guidlines."
-            } else if place == "gas station"{
-                riskkResult.text = "You're at a gas station in a high risk area, please still be cautious and sanitize your hands after touching pumps or purchasing items from inside."
-            } else if place == "restaurant"{
-                riskkResult.text = "You're at a restaurant in a high risk area, please keep socially distanced from others and wear a mask when speaking to employees and others."
-            } else if place == "dealership"{
-                riskkResult.text = "You're at a dealership in a high risk area, please wear a mask at all times and santitize hands often."
-            } else if place == "hotel"{
-                riskkResult.text = "You're at a hotel in a high risk area, please wear a mask when you are not in your and sanitize often."
-            } else if place == "hospital"{
-                riskkResult.text = "You're at a hospital in a high risk area, please be cautious and follow hospital COVID-19 guidlines as this area is very high risk."
-            } else if place == "gym"{
-                riskkResult.text = "You're at a gym in a high risk area, please still be sure to take normal covid precautions."
-            } else if place == "airport"{
-                riskkResult.text = "You're at a airport in a high risk area, please be cautious and follow airport COVID-19 guidlines as this area is very high risk."
-            } else if place == "bank"{
-                riskkResult.text = "You're at a bank in a high risk area, please keep socially distanced from others and wear a mask when speaking to employees and others."
-            } else if place == "church"{
-                riskkResult.text = "You're at a church in a high risk area, please wear a mask at all times and socially distace from others while keeping hands sanitized."
-            } else if place == "doctor"{
-                riskkResult.text = "You're at a doctor in a high risk area, please be cautious and follow companies COVID-19 guidlines as this area is very high risk."
-            } else if place == "stadium"{
-                riskkResult.text = "You're at a stadium in a high risk area, please keep socially ditanced and santitize often."
-            } else if place == "supermarket"{
-                riskkResult.text = "You're at a supermarket in a high risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            } else if place == "store"{
-                riskkResult.text = "You're at a store in a high risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            } else if place == "establishment"{
-                riskkResult.text = "You're in an establishment in a high risk area, please keep socially distanced and keep hands sanitized."
-            } else if place == "museum"{
-                riskkResult.text = "You're at a museum in a high risk area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
-            }
+        }
+        
+        //display what type of place the user is in and tell precautions
+        if place == "university"{
+            riskkResult.text = "You're at a university in a \(risk) area, please be sure to follow campus COVID-19 guidlines."
+        } else if place == "gas station"{
+            riskkResult.text = "You're at a gas station in a \(risk) area, please be cautious and sanitize your hands after touching pumps or purchasing items from inside."
+        } else if place == "restaurant"{
+            riskkResult.text = "You're at a restaurant in a \(risk) area, please keep socially distanced from others and wear a mask when speaking to employees and others."
+        } else if place == "dealership"{
+            riskkResult.text = "You're at a dealership in a \(risk) area, please wear a mask at all times and santitize hands often."
+        } else if place == "hotel"{
+            riskkResult.text = "You're at a hotel in a \(risk) area, please wear a mask when you are not in your and sanitize often."
+        } else if place == "hospital"{
+            riskkResult.text = "You're at a hospital in a \(risk) area, please be cautious and follow hospital COVID--19 guidlines as this area is usually very high risk."
+        } else if place == "gym"{
+            riskkResult.text = "You're at a gym in a \(risk) area, please still be sure to take normal covid precautions."
+        } else if place == "airport"{
+            riskkResult.text = "You're at an airport in a \(risk) area, please be cautious and follow airport COVID-19 guidlines as this area is usually very high risk."
+        } else if place == "bank"{
+            riskkResult.text = "You're at a bank in a \(risk) area, please keep socially distanced from others and wear a mask when speaking to employees and others."
+        } else if place == "church"{
+            riskkResult.text = "You're at a church in a \(risk) area, please wear a mask at all times and socially distace from others while keeping hands sanitized."
+        } else if place == "doctor"{
+            riskkResult.text = "You're at a hospital in a \(risk) area, please be cautious and follow companies COVID--19 guidlines as this area is usually very high risk."
+        } else if place == "stadium"{
+            riskkResult.text = "You're at a stadium in a \(risk) area, please keep socially ditanced and santitized often."
+        } else if place == "supermarket"{
+            riskkResult.text = "You're at a supermarket in a \(risk) area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
+        } else if place == "store"{
+            riskkResult.text = "You're at a store in a \(risk) area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
+        } else if place == "establishment"{
+            riskkResult.text = "You're in an establishment in a \(risk) area, please keep socially distanced and keep hands sanitized."
+        } else if place == "museum"{
+            riskkResult.text = "You're at a museum in a \(risk) area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
+        } else {
+            riskkResult.text = "You're at a \(risk) area, please wear mask at all times and keep socially distanced while keeping hands sanitized."
         }
     }
 }
